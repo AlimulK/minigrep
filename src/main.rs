eb1997e134c6ac3e91ce70f78cbbfb65733e5c2f
AlimulK <samskarim@gmail.com>
2022-05-20 22:22:26 +0100

    functioning case sensitive search

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env; // for taking in args (only works on unicode)
 use std::process; // for error handling
+
 use minigrep::Config;
 
 
@@ -8,7 +9,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Issue parsing arguments: {}", err);
+        println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
