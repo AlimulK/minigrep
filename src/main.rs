ad2c3f8ea358ff633ccab118c59980fb7baf564e
AlimulK <samskarim@gmail.com>
2022-05-20 12:57:46 +0100

    taking in args

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
-
+use std::env;
+// this only works for unicode
 
 fn main() {
-    println!("Hello, world!");
+    // taking in the arguments
+    let args: Vec<String> = env::args().collect();
+
+    // saving the arguments to variables
+    let query = &args[1];
+    let filename = &args[2];
+    // [0] would be the program itself (minigrep)
+
 }
