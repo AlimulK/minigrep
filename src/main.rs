ac65d297b7bbac6e61806fb3a1eb8d03a8e48c02
AlimulK <samskarim@gmail.com>
2022-05-20 17:29:13 +0100

    error handling

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,34 @@
 use std::env; // for taking in args (only works on unicode)
 use std::fs; // for manipulating files
+use std::process; // for error handling
 
 fn main() {
     // taking in the arguments
     let args: Vec<String> = env::args().collect();
 
-    // saving the arguments to variables
-    let query = &args[1];
-    let filename = &args[2];
-    // [0] would be the program itself (minigrep)
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Issue parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let content = fs::read_to_string(filename)
+    let content = fs::read_to_string(config.filename)
         .expect("Couldn't read the file properly");
+}
+
+// I like to keep my class functions without whitespace
+struct Config {
+    query: String,
+    filename: String,
+}
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
 }
